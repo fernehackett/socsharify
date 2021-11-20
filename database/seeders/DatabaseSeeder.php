a3a4b4d452c540f982b1218d015319399fbbf020
Nic <tmninh94@gmail.com>
2021-11-20 19:23:21 +0700

    "Nic"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        $this->call(users::class);
+        $this->call(create_plan_data::class);
         $this->call(insert_default_templates::class);
 
         if (config('variables.WITH_FAKER')) {
